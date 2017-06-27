72f1159a1cb03198913269f715562daa60ae0b1f
Andy Haden <andyh2@me.com>
2017-06-26 21:05:32 -0700

    cabana: url parameters

--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import CanExplorer from './CanExplorer';
 import AcuraDbc from './acura-dbc';
+import {getUrlParameter} from './utils/url';
 
 import './index.css';
 
 ReactDOM.render(
   <CanExplorer
-     dongleId={"3a874b7845c28583"}
-     name={"2017-06-09--18-23-30"} />,
+     dongleId={getUrlParameter('route') ? undefined : "3a874b7845c28583"}
+     name={getUrlParameter('route') || "2017-06-09--18-23-30"} />,
   document.getElementById('root')
 );
