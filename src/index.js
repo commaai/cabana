394a2081c8da3e3f380f4e9242a0b5508bf5db95
Andy Haden <andyh2@me.com>
2017-06-27 16:56:24 -0700

    cabana: route param is full name

--- src/index.js
+++ src/index.js
@@ -6,9 +6,19 @@ import {getUrlParameter} from './utils/url';
 
 import './index.css';
 
+const routeFullName = getUrlParameter('route');
+let props = {};
+if(routeFullName) {
+    const [dongleId, route] = routeFullName.split('|');
+    props.dongleId = dongleId;
+    props.name = route;
+} else {
+    props.dongleId = '3a874b7845c28583';
+    props.name = '2017-06-09--18-23-30';
+}
+
 ReactDOM.render(
   <CanExplorer
-     dongleId={getUrlParameter('route') ? undefined : "3a874b7845c28583"}
-     name={getUrlParameter('route') || "2017-06-09--18-23-30"} />,
+     {...props} />,
   document.getElementById('root')
 );
