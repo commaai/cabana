8e8a2e2fc42377383736f301d292a5298c7041fd
Andy Haden <andyh2@me.com>
2017-06-27 20:07:58 -0700

    cabana: tesla dbc test

--- src/__tests__/can/dbc.write.test.js
+++ src/__tests__/can/dbc.write.test.js
@@ -1,8 +1,14 @@
 import DBC, {swapOrder} from '../../models/can/dbc';
 import {ACURA_DBC} from '../res/acura-dbc';
-
+import {TESLA_DBC} from '../res/tesla-dbc';
 test('DBC.text() for acura DBC should be equivalent to its original text', () => {
     const dbc = new DBC(ACURA_DBC);
 
     expect(dbc.text()).toBe(ACURA_DBC);
+});
+
+test('DBC.text() for tesla DBC should be equivalent to its origianl text', () => {
+    const dbc = new DBC(TESLA_DBC);
+
+    expect(dbc.text()).toBe(TESLA_DBC);
 });
