f7fb7b693605f2c20539cb8082d8ece41dc99b87
espes <espes@pequalsnp.com>
2017-07-03 15:22:07 -0700

    cabana: deploy script

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+
+eval "$(~/one/tools/azure/export_key.py chffrdist)"
+cd "$(dirname $0)"
+
+npm run build
+
+pushd build/
+find . -type f | while read f; do
+  azure storage blob upload -q "$f" cabana "$f"
+done
+popd
+
+pushd public
+find img -type f | while read f; do
+  azure storage blob upload -q "$f" cabana "$f"
+done
+popd
