bec177a4339a8b36461b63e247d52b07e50f60da
ChristopherBiscardi <chris@christopherbiscardi.com>
2017-12-14 14:01:36 -0800

    set up tests for react 16/enzyme

--- src/components/LoadingBar.js
+++ src/components/LoadingBar.js
@@ -1,42 +1,36 @@
 import React, { Component } from "react";
-import { css, StyleSheet } from "aphrodite/no-important";
+import styled, { keyframes } from "react-emotion";
 
-const keyframes = {
-  "0%": {
-    transform: "translateX(0)"
-  },
-  to: {
-    transform: "translateX(-400px)"
+const frames = keyframes`
+  0% {
+    transform: translateX(0)
   }
-};
-const animationColor1 = "RGBA(74, 242, 161, 1.00)";
-const animationColor2 = "RGBA(140, 169, 197, 1.00)";
+  to {
+    transform: translateX(-400px)
+  }
+`;
+const animationColor1 = "rgba(74, 242, 161, 1.00)";
+const animationColor2 = "rgba(140, 169, 197, 1.00)";
 
-const Styles = StyleSheet.create({
-  loadingBar: {
-    display: "block",
-    animationName: [keyframes],
-    animationDuration: "2s",
-    animationTimingFunction: "linear",
-    animationIterationCount: "infinite",
-    backgroundColor: animationColor1,
-    backgroundImage: `linear-gradient(to right,
-                                          ${animationColor2} 0,
-                                          ${animationColor2} 50%,
-                                          ${animationColor1} 50%,
-                                          ${animationColor1} 100%)`,
-    backgroundRepeat: "repeat-x",
-    backgroundSize: "25pc 25pc",
-    width: "200%",
-    position: "fixed",
-    top: 0,
-    left: 0,
-    height: 2
-  }
-});
-
-export default class LoadingBar extends Component {
-  render() {
-    return <div className={css(Styles.loadingBar)} />;
-  }
-}
+export default styled("div")`
+  display: block;
+  animation-name: ${frames};
+  animation-duration: 2s;
+  animation-timing-function: linear;
+  animation-iteration-count: infinite;
+  background-color: ${animationColor1};
+  background-image: linear-gradient(
+    to right,
+    ${animationColor2} 0,
+    ${animationColor2} 50%,
+    ${animationColor1} 50%,
+    ${animationColor1} 100%
+  );
+  background-repeat: repeat-x;
+  background-size: 25pc 25pc;
+  width: 200%;
+  position: fixed;
+  top: 0;
+  left: 0;
+  height: 2;
+`;
