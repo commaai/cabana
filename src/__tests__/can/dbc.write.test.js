a809f733f351b434b43a1de7687479c09a071979
ChristopherBiscardi <chris@christopherbiscardi.com>
2017-12-15 13:20:07 -0800

    rename dbc files to not get picked up as tests

--- src/__tests__/can/dbc.write.test.js
+++ src/__tests__/can/dbc.write.test.js
@@ -1,6 +1,9 @@
 import DBC, { swapOrder } from "../../models/can/dbc";
-import { ACURA_DBC } from "../res/acura-dbc";
-import { TESLA_DBC } from "../res/tesla-dbc";
+import fs from "fs";
+import path from "path";
+
+const ACURA_DBC = fs.readFileSync("src/__tests__/res/acura.dbc", "utf-8");
+//import { TESLA_DBC } from "../res/tesla.dbc";
 
 test("DBC.text() for acura DBC should be equivalent to its original text", () => {
   const dbc = new DBC(ACURA_DBC);
