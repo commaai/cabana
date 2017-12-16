2634c84f7ef5d82e955113f3c65e9fa78a3505a6
ChristopherBiscardi <chris@christopherbiscardi.com>
2017-12-15 16:04:43 -0800

    azure is az on travis

--- scripts/travis-deploy.sh
+++ scripts/travis-deploy.sh
@@ -5,12 +5,12 @@ cd $TRAVIS_BUILD_DIR
 
 pushd build/
 find . -not -name "*.map" -type f | while read f; do
-  azure storage blob upload -q "$f" cabana "$f"
+  az storage blob upload -q "$f" cabana "$f"
 done
 popd
 
 pushd public
 find img -type f | while read f; do
-  azure storage blob upload -q "$f" cabana "$f"
+  az storage blob upload -q "$f" cabana "$f"
 done
 popd
