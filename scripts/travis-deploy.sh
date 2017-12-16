5c6b9999e0a71f36da1db6bb2505f982b0e7be25
ChristopherBiscardi <chris@christopherbiscardi.com>
2017-12-15 16:20:45 -0800

    -q doesnt exist in az v2

--- scripts/travis-deploy.sh
+++ scripts/travis-deploy.sh
@@ -5,12 +5,12 @@ cd $TRAVIS_BUILD_DIR
 
 pushd build/
 find . -not -name "*.map" -type f | while read f; do
-  az storage blob upload -q --file "$f" --container-name cabana --name "$f"
+  az storage blob upload --file "$f" --container-name cabana --name "$f"
 done
 popd
 
 pushd public
 find img -type f | while read f; do
-  az storage blob upload -q --file "$f" --container-name cabana --name "$f"
+  az storage blob upload --file "$f" --container-name cabana --name "$f"
 done
 popd
