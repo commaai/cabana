fdf4bfe44b293c0ad242c6e530bf528ca198ca27
ChristopherBiscardi <chris@christopherbiscardi.com>
2017-12-18 14:29:42 -0800

    decline stats generation

--- scripts/travis-deploy-deprecated.sh
+++ scripts/travis-deploy-deprecated.sh
@@ -3,6 +3,9 @@ set -e
 
 cd $TRAVIS_BUILD_DIR
 
+# decline stats generation
+yes n | azure help
+
 pushd build/
 find . -not -name "*.map" -type f | while read f; do
   azure storage blob upload -q "$f" cabana "$f"
