347c3fdfa772faeae8568082afdb443c1a3bd5bb
Chris Vickery <chrisinajar@gmail.com>
2019-10-03 12:47:08 -0700

    Remove t endpoint redundancy for shared rides (#25)
    * Remove t endpoint redundancy for shared rides
    
    This reverts commit a973409d87f12f71918e4f5450817ac9be65c72c.
    
    * Remove more dead code
    
    * Remove api/can

--- src/workers/dbc-csv-downloader.worker.js
+++ src/workers/dbc-csv-downloader.worker.js
@@ -3,95 +3,12 @@
 // import Sentry from '../logging/Sentry';
 
 import NumpyLoader from "../utils/loadnpy";
-import * as CanApi from "../api/can";
 
 const MAX_CONNECTIONS = 8;
 
 var window = self;
 
 const Int64LE = require("int64-buffer").Int64LE;
-
-function transformData(data) {}
-async function fetchAndPostData(
-  base,
-  currentPart,
-  [minPart, maxPart],
-  canStartTime
-) {
-  console.log("starting fetchAndPostData process");
-
-  var partList = [];
-  minPart = currentPart;
-  var prevPart = Promise.resolve();
-  var promiseBuffer = [];
-  var totalParts = maxPart - minPart;
-  var currentProcess = 0;
-  while (currentPart <= maxPart) {
-    // post inc, pass the previous value then inc
-    console.log("Starting download", currentPart - minPart, "of", totalParts);
-    prevPart = downloadNextPart(
-      base,
-      canStartTime,
-      currentProcess,
-      prevPart,
-      currentPart++
-    );
-    currentProcess = ~~(100 * ((currentPart - minPart) / totalParts));
-
-    promiseBuffer.push(prevPart);
-    if (promiseBuffer.length > MAX_CONNECTIONS) {
-      await promiseBuffer.shift();
-    }
-  }
-
-  await Promise.all(promiseBuffer);
-  // processing is done!
-  self.postMessage({
-    progress: 100,
-    shouldClose: true
-  });
-  self.close();
-}
-
-async function downloadNextPart(
-  base,
-  canStartTime,
-  currentProcess,
-  prevPartPromise,
-  currentPart
-) {
-  var awaitedData = await CanApi.fetchCanPart(base, currentPart);
-  await prevPartPromise;
-
-  const { times, sources, addresses, datas } = awaitedData;
-
-  // times is a float64array, which we want to be a normal array for now
-  const logData = [].slice
-    .call(times)
-    .map((t, i) => {
-      var src = Int64LE(sources, i * 8).toString(10);
-      var address = Int64LE(addresses, i * 8);
-      var addressHexStr = address.toString(16);
-      var id = src + ":" + addressHexStr;
-
-      var addressNum = address.toNumber();
-      var data = datas.slice(i * 8, (i + 1) * 8);
-
-      return `${t - canStartTime},${addressNum},${src},${Buffer.from(
-        data
-      ).toString("hex")}\n`;
-    })
-    .join("");
-
-  console.log("posting message");
-  self.postMessage({
-    progress: currentProcess,
-    logData,
-    shouldClose: false
-  });
-
-  return awaitedData;
-}
 
 function transformAndSend(rawData) {
   var totalSize = 0;
@@ -266,7 +183,10 @@ self.onmessage = function(e) {
     console.log("Using raw data from memory", canStartTime);
     transformAndSend(data, canStartTime);
   } else {
-    console.log("Fetching data using CanApi");
-    fetchAndPostData(base, parts[0], parts, canStartTime);
+    self.postMessage({
+      progress: 100,
+      shouldClose: true
+    });
+    self.close();
   }
 };
