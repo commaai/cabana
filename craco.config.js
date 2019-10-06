0f47074e14524583f17e904a1bd1d41ad80a5b96
Chris Vickery <chrisinajar@gmail.com>
2019-10-06 14:10:45 -0700

    Add sentry and fix potential minification bugs

--- craco.config.js
+++ craco.config.js
@@ -1,17 +1,32 @@
 const WorkerLoaderPlugin = require("craco-worker-loader");
+const SentryPlugin = require("craco-sentry-plugin");
 
 module.exports = function({ env }) {
+  var plugins = [
+    {
+      plugin: WorkerLoaderPlugin
+    }
+  ];
+  if (env === "production") {
+    plugins.push({
+      plugin: SentryPlugin
+    });
+  }
   return {
-    plugins: [
-      {
-        plugin: WorkerLoaderPlugin
-      }
-    ],
+    plugins: plugins,
     webpack: {
-      configure: {
-        output: {
-          globalObject: "this"
-        }
+      configure: (webpackConfig, { env, paths }) => {
+        webpackConfig.output.globalObject = "this";
+        webpackConfig.optimization.minimizer = webpackConfig.optimization.minimizer.map(
+          function(plugin) {
+            if (plugin.constructor.name !== "TerserPlugin") {
+              return plugin;
+            }
+            plugin.options.terserOptions.keep_fnames = true;
+            return plugin;
+          }
+        );
+        return webpackConfig;
       }
     }
   };
