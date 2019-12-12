d95ddca99fbaeaf8a6639503e22c687414e5b50f
Andy Haden <andy@comma.ai>
2019-12-11 17:56:47 -0800

    Fix test

--- src/__tests__/demo.test.js
+++ src/__tests__/demo.test.js
@@ -1,6 +1,14 @@
 /* eslint-env jest */
 import { demoLogUrls, demoProps, demoRoute } from '../demo';
 import { getLogPart } from '../api/rlog';
+import { StyleSheetTestUtils } from 'aphrodite';
+
+beforeEach(() => {
+  StyleSheetTestUtils.suppressStyleInjection();
+});
+afterEach(() => {
+  StyleSheetTestUtils.clearBufferAndResumeStyleInjection();
+});
 
 describe('demo fixtures', () => {
   it('demo log urls are valid', async () => {
