99b82ecef80e8ea2bca771f6a82dbfb2d4111489
Joost Wooning <jwooning@gmail.com>
2021-09-14 12:46:51 +0200

    Updates

---
+++ precache-manifest.c890d58c21fb99b3134699694bd66d7f.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67936ab73453a60abf1468f83b131dae",
+    "url": "/cabana/2c1fb3ea45a56ca7bce0.worker.js"
+  },
+  {
+    "revision": "882b94771aea5e714c932f6ff3212e4c",
+    "url": "/cabana/74c3ae30d3c3fa2d6e9d.worker.js"
+  },
+  {
+    "revision": "bcec0f6e027e7c6d4dcf4889a8e6327b",
+    "url": "/cabana/bf7e1b7ce39f76028dc4.worker.js"
+  },
+  {
+    "revision": "88ef181cadb187fae8bd5f8db48451a0",
+    "url": "/cabana/fdbfd08ff19fe7bdfbad.worker.js"
+  },
+  {
+    "revision": "2118ba62be4fd08816b386f8829a700d",
+    "url": "/cabana/index.html"
+  },
+  {
+    "revision": "87dce2d774d575924492",
+    "url": "/cabana/static/css/main.9ce31ab0.chunk.css"
+  },
+  {
+    "revision": "5fed63e58b3244b32c6b",
+    "url": "/cabana/static/js/2.aef2797c.chunk.js"
+  },
+  {
+    "revision": "87dce2d774d575924492",
+    "url": "/cabana/static/js/main.af939a75.chunk.js"
+  },
+  {
+    "revision": "f7f907c00ca18da482e8",
+    "url": "/cabana/static/js/runtime-main.ec09f551.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/cabana/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/cabana/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/cabana/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/cabana/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/cabana/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "e5a08e76f61c8ef370034956eb5865e1",
+    "url": "/cabana/static/media/webusb-enable-experimental-features.e5a08e76.png"
+  },
+  {
+    "revision": "aceef6a08425cf752843a9dabd208c56",
+    "url": "/cabana/static/media/webusb-enable-webusb.aceef6a0.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/cabana/precache-manifest.430c37a45bd6152dacd048ad1d5586ae.js"
+  "/cabana/precache-manifest.c890d58c21fb99b3134699694bd66d7f.js"
 );
 
 self.addEventListener('message', (event) => {
